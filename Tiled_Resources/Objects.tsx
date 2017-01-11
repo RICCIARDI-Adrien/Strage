8ce22037dd27eb3f545039a7f711f65d6c50a626
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2017-01-11 14:35:36 +0100

    Moved Tiled-specific resources to a dedicated directory.

---
+++ Tiled_Resources/Objects.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Objects" tilewidth="64" tileheight="64" tilecount="3">
+ <tile id="0">
+  <image width="64" height="64" source="Player.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="Medipack.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="Ammunition.png"/>
+ </tile>
+</tileset>
