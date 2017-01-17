c4065eefd89ba005f68831df4e244223a8225252
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2017-01-17 16:14:51 +0100

    Added scene tileset, expanded test level.

---
+++ Tiled_Resources/Scene.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Floors" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+ <tile id="0">
+  <image width="64" height="64" source="../Textures/Floor_Dirt_0.bmp"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../Textures/Floor_Dirt_1.bmp"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../Textures/Floor_Grass_0.bmp"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../Textures/Floor_Grass_1.bmp"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../Textures/Floor_Metal_Grid_0.bmp"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../Textures/Floor_Metal_Plate_0.bmp"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../Textures/Floor_Metal_Plate_1.bmp"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../Textures/Wall_Stone_0.bmp"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../Textures/Wall_White_Bricks_0.bmp"/>
+ </tile>
+</tileset>
