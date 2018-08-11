8a3013c60a410cd3fce203141b00e476dced7366
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2018-08-11 11:25:29 +0200

    Moved StaticEntityAnimatedTexture class implementation to a dedicated source file.

--- Includes/StaticEntityAnimatedTexture.hpp
+++ Includes/StaticEntityAnimatedTexture.hpp
@@ -5,13 +5,10 @@
 #define HPP_STATIC_ENTITY_ANIMATED_TEXTURE_HPP
 
 #include <AnimatedTexture.hpp>
-#include <Log.hpp>
-#include <Renderer.hpp>
 #include <StaticEntity.hpp>
-#include <TextureManager.hpp>
 
 /** @class StaticEntityAnimatedTexture
- * Display several textures in sequence and stop.
+ * Display several textures in sequence then stop.
  */
 class StaticEntityAnimatedTexture: public StaticEntity
 {
@@ -23,33 +20,21 @@ class StaticEntityAnimatedTexture: public StaticEntity
 		/** Spawn an animated texture at the requested map location.
 		 * @param x X coordinate on the map.
 		 * @param y Y coordinate on the map.
-		 * @param pointerTexture The texture to use.
+		 * @param pointerTexture The texture to use. This must be an AnimatedTexture dynamically created (for instance using TextureManager::createAnimatedTextureFromId()).
 		 */
-		StaticEntityAnimatedTexture(int x, int y, Texture *pointerTexture): StaticEntity(x, y, pointerTexture)
-		{
-			_isAnimationTerminated = 0;
-		}
+		StaticEntityAnimatedTexture(int x, int y, AnimatedTexture *pointerTexture);
 		
-		/** Free allocated resources. */
-		virtual ~StaticEntityAnimatedTexture()
-		{
-			delete _pointerTexture;
-		}
+		// No need for documentation because it is the same as parent function
+		virtual ~StaticEntityAnimatedTexture();
 		
-		virtual void render()
-		{
-			// Display the animation current image
-			if (_pointerTexture->render(_positionRectangle.x - Renderer::displayX, _positionRectangle.y - Renderer::displayY) != 0) _isAnimationTerminated = 1;
-		}
+		// No need for documentation because it is the same as parent function
+		virtual void render();
 		
-		/** Update the _pointerTexture with the right texture when time has come. TODO
+		/** Tell whether the animation finished playing.
 		 * @return 0 while the animation must be displayed,
 		 * @return 1 when the animation is finished and the object must be destroyed.
 		 */
-		virtual int update()
-		{
-			return _isAnimationTerminated;
-		}
+		virtual int update();
 };
 
 #endif
