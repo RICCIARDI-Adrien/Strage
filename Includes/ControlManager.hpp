675ba97c52011e3895c98839a1db271778703e42
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-05-04 17:27:10 +0200

    Refactored 'ControlManager' namespace.

--- Includes/ControlManager.hpp
+++ Includes/ControlManager.hpp
@@ -9,59 +9,51 @@
 
 namespace ControlManager
 {
+	/** All handled keys. Keyboard and game controller keys are translated to these virtual key codes. */
+	typedef enum
+	{
+		KEY_ID_GO_UP,
+		KEY_ID_GO_DOWN,
+		KEY_ID_GO_LEFT,
+		KEY_ID_GO_RIGHT,
+		KEY_ID_PRIMARY_SHOOT,
+		KEY_ID_SECONDARY_SHOOT,
+		KEY_ID_PAUSE_GAME,
+		KEY_ID_MENU_SELECT,
+		KEY_IDS_COUNT
+	} KeyId;
 
-//-------------------------------------------------------------------------------------------------
-// Types
-//-------------------------------------------------------------------------------------------------
-/** All handled keys. Keyboard and game controller keys are translated to these virtual key codes. */
-typedef enum
-{
-	KEY_ID_GO_UP,
-	KEY_ID_GO_DOWN,
-	KEY_ID_GO_LEFT,
-	KEY_ID_GO_RIGHT,
-	KEY_ID_PRIMARY_SHOOT,
-	KEY_ID_SECONDARY_SHOOT,
-	KEY_ID_PAUSE_GAME,
-	KEY_ID_MENU_SELECT,
-	KEY_IDS_COUNT
-} KeyId;
+	/** Initialize the first found game controller.
+	* @return 0 if the first found game controller was successfully initialized or if no game controller was detected,
+	* @return -1 if a game controller was detected but could not be initialized.
+	*/
+	int initialize(void);
 
-//-------------------------------------------------------------------------------------------------
-// Functions
-//-------------------------------------------------------------------------------------------------
-/** Initialize the first found game controller.
- * @return 0 if the first found game controller was successfully initialized or if no game controller was detected,
- * @return -1 if a game controller was detected but could not be initialized.
- */
-int initialize(void);
+	/** Close any previously opened game controller. */
+	void uninitialize(void);
 
-/** Close any previously opened game controller. */
-void uninitialize(void);
+	/** Tell if a key is pressed or released.
+	* @param keyId The key to know state.
+	* @return true if the key is pressed,
+	* @return false if the key is released.
+	*/
+	bool isKeyPressed(KeyId keyId);
 
-/** Tell if a key is pressed or released.
- * @param keyId The key to know state.
- * @return 1 if the key is pressed,
- * @return 0 if the key is released.
- */
-int isKeyPressed(KeyId keyId);
+	/** Handle a SDL event related to the keyboard.
+	* @param pointerEvent The keyboard event.
+	*/
+	void handleKeyboardEvent(SDL_Event *pointerEvent);
 
-/** Handle a SDL event related to the keyboard.
- * @param pointerEvent The keyboard event.
- */
-void handleKeyboardEvent(SDL_Event *pointerEvent);
+	/** Handle a SDL event related to the game controller.
+	* @param pointerEvent The game controller event.
+	*/
+	void handleGameControllerEvent(SDL_Event *pointerEvent);
 
-/** Handle a SDL event related to the game controller.
- * @param pointerEvent The game controller event.
- */
-void handleGameControllerEvent(SDL_Event *pointerEvent);
-
-/** Tell if the last pressed direction key of the current controller was on the vertical or the horizontal axis.
- * @return 1 if the direction was vertical,
- * @return 0 if the direction was horizontal.
- */
-int isLastPressedDirectionKeyOnVerticalAxis();
-
+	/** Tell if the last pressed direction key of the current controller was on the vertical or the horizontal axis.
+	* @return true if the direction was vertical,
+	* @return false if the direction was horizontal.
+	*/
+	bool isLastPressedDirectionKeyOnVerticalAxis();
 }
 
 #endif
--- Sources/ControlManager.cpp
+++ Sources/ControlManager.cpp
@@ -9,269 +9,253 @@
 
 namespace ControlManager
 {
+	/** Hold all keys state. */
+	static bool _isKeyPressed[KEY_IDS_COUNT] = { false };
+	/** Tell if the last pressed direction key was on the vertical or the horizontal axis. */
+	static bool _isLastPressedDirectionKeyOnVerticalAxis = false;
 
-//-------------------------------------------------------------------------------------------------
-// Private variables
-//-------------------------------------------------------------------------------------------------
-/** Hold all keys state. */
-static int _isKeyPressed[KEY_IDS_COUNT] = {0};
-/** Tell if the last pressed direction key was on the vertical or the horizontal axis. */
-static int _isLastPressedDirectionKeyOnVerticalAxis = 0;
+	/** The detected game controller. */
+	static SDL_GameController *_pointerGameController = NULL;
 
-/** The detected game controller. */
-static SDL_GameController *_pointerGameController = NULL;
-
-//-------------------------------------------------------------------------------------------------
-// Private functions
-//-------------------------------------------------------------------------------------------------
-/** What to do when the "go up" key is pressed. */
-static void _handleGoUpKeyPress()
-{
-	_isKeyPressed[KEY_ID_GO_UP] = 1;
-	_isKeyPressed[KEY_ID_GO_DOWN] = 0;
-	_isLastPressedDirectionKeyOnVerticalAxis = 1;
-}
-
-/** What to do when the "go down" key is pressed. */
-static void _handleGoDownKeyPress()
-{
-	_isKeyPressed[KEY_ID_GO_UP] = 0;
-	_isKeyPressed[KEY_ID_GO_DOWN] = 1;
-	_isLastPressedDirectionKeyOnVerticalAxis = 1;
-}
-
-/** What to do when the "go left" key is pressed. */
-static void _handleGoLeftKeyPress()
-{
-	_isKeyPressed[KEY_ID_GO_LEFT] = 1;
-	_isKeyPressed[KEY_ID_GO_RIGHT] = 0;
-	_isLastPressedDirectionKeyOnVerticalAxis = 0;
-}
-
-/** What to do when the "go right" key is pressed. */
-static void _handleGoRightKeyPress()
-{
-	_isKeyPressed[KEY_ID_GO_LEFT] = 0;
-	_isKeyPressed[KEY_ID_GO_RIGHT] = 1;
-	_isLastPressedDirectionKeyOnVerticalAxis = 0;
-}
-
-//-------------------------------------------------------------------------------------------------
-// Public functions
-//-------------------------------------------------------------------------------------------------
-int initialize(void)
-{
-	int i, Detected_Joysticks_Count;
-	
-	// Try to open the first detected game controller
-	Detected_Joysticks_Count = SDL_NumJoysticks();
-	for (i = 0; i < Detected_Joysticks_Count; i++)
+	/** What to do when the "go up" key is pressed. */
+	static void _handleGoUpKeyPress()
 	{
-		// Make sure device is supported by SDL game controller API
-		if (!SDL_IsGameController(i)) continue;
-	
-		// Gain access to controller
-		_pointerGameController = SDL_GameControllerOpen(i);
-		if (_pointerGameController == NULL) LOG_ERROR("Failed to open game controller with ID %d (%s).", i, SDL_GetError());
+		_isKeyPressed[KEY_ID_GO_UP] = true;
+		_isKeyPressed[KEY_ID_GO_DOWN] = false;
+		_isLastPressedDirectionKeyOnVerticalAxis = true;
+	}
+
+	/** What to do when the "go down" key is pressed. */
+	static void _handleGoDownKeyPress()
+	{
+		_isKeyPressed[KEY_ID_GO_UP] = false;
+		_isKeyPressed[KEY_ID_GO_DOWN] = true;
+		_isLastPressedDirectionKeyOnVerticalAxis = true;
+	}
+
+	/** What to do when the "go left" key is pressed. */
+	static void _handleGoLeftKeyPress()
+	{
+		_isKeyPressed[KEY_ID_GO_LEFT] = true;
+		_isKeyPressed[KEY_ID_GO_RIGHT] = false;
+		_isLastPressedDirectionKeyOnVerticalAxis = false;
+	}
+
+	/** What to do when the "go right" key is pressed. */
+	static void _handleGoRightKeyPress()
+	{
+		_isKeyPressed[KEY_ID_GO_LEFT] = false;
+		_isKeyPressed[KEY_ID_GO_RIGHT] = true;
+		_isLastPressedDirectionKeyOnVerticalAxis = false;
+	}
+
+	int initialize(void)
+	{
+		// Try to open the first detected game controller
+		int Detected_Joysticks_Count = SDL_NumJoysticks();
+		for (int i = 0; i < Detected_Joysticks_Count; i++)
+		{
+			// Make sure device is supported by SDL game controller API
+			if (!SDL_IsGameController(i)) continue;
+		
+			// Gain access to controller
+			_pointerGameController = SDL_GameControllerOpen(i);
+			if (_pointerGameController == NULL) LOG_ERROR("Failed to open game controller with ID %d (%s).", i, SDL_GetError());
+			else
+			{
+				LOG_INFORMATION("Successfully detected game controller '%s'.", SDL_GameControllerName(_pointerGameController));
+				break;
+			}
+		}
+		
+		return 0;
+	}
+
+	void uninitialize(void)
+	{
+		// Close the opened game controller
+		if ((_pointerGameController != NULL) && (SDL_GameControllerGetAttached(_pointerGameController))) SDL_GameControllerClose(_pointerGameController);
+	}
+
+	bool isKeyPressed(KeyId keyId)
+	{
+		// Tell that a non-existing key is released
+		if (keyId >= KEY_IDS_COUNT) return false;
+		
+		return _isKeyPressed[keyId];
+	}
+
+	void handleKeyboardEvent(SDL_Event *pointerEvent)
+	{
+		// Handle key press
+		if (pointerEvent->key.state == SDL_PRESSED)
+		{
+			switch (pointerEvent->key.keysym.scancode)
+			{
+				case SDL_SCANCODE_UP:
+					_handleGoUpKeyPress();
+					break;
+					
+				case SDL_SCANCODE_DOWN:
+					_handleGoDownKeyPress();
+					break;
+					
+				case SDL_SCANCODE_LEFT:
+					_handleGoLeftKeyPress();
+					break;
+					
+				case SDL_SCANCODE_RIGHT:
+					_handleGoRightKeyPress();
+					break;
+					
+				case SDL_SCANCODE_SPACE:
+					_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = true;
+					break;
+					
+				case SDL_SCANCODE_LCTRL:
+					_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = true;
+					break;
+					
+				case SDL_SCANCODE_ESCAPE:
+					_isKeyPressed[KEY_ID_PAUSE_GAME] = true;
+					break;
+					
+				case SDL_SCANCODE_RETURN:
+					_isKeyPressed[KEY_ID_MENU_SELECT] = true;
+					break;
+					
+				default:
+					break;
+			}
+		}
+		// Handle key release
 		else
 		{
-			LOG_INFORMATION("Successfully detected game controller '%s'.", SDL_GameControllerName(_pointerGameController));
-			break;
+			switch (pointerEvent->key.keysym.scancode)
+			{
+				case SDL_SCANCODE_UP:
+					_isKeyPressed[KEY_ID_GO_UP] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = false;
+					break;
+					
+				case SDL_SCANCODE_DOWN:
+					_isKeyPressed[KEY_ID_GO_DOWN] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = false;
+					break;
+					
+				case SDL_SCANCODE_LEFT:
+					_isKeyPressed[KEY_ID_GO_LEFT] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = true;
+					break;
+					
+				case SDL_SCANCODE_RIGHT:
+					_isKeyPressed[KEY_ID_GO_RIGHT] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = true;
+					break;
+					
+				case SDL_SCANCODE_SPACE:
+					_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = false;
+					break;
+					
+				case SDL_SCANCODE_LCTRL:
+					_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = false;
+					break;
+				
+				case SDL_SCANCODE_ESCAPE:
+					_isKeyPressed[KEY_ID_PAUSE_GAME] = false;
+					break;
+					
+				case SDL_SCANCODE_RETURN:
+					_isKeyPressed[KEY_ID_MENU_SELECT] = false;
+					break;
+					
+				default:
+					break;
+			}
 		}
 	}
-	
-	return 0;
-}
 
-void uninitialize(void)
-{
-	// Close the opened game controller
-	if ((_pointerGameController != NULL) && (SDL_GameControllerGetAttached(_pointerGameController))) SDL_GameControllerClose(_pointerGameController);
-}
-
-int isKeyPressed(KeyId keyId)
-{
-	// Tell that a non-existing key is released
-	if (keyId >= KEY_IDS_COUNT) return 0;
-	
-	return _isKeyPressed[keyId];
-}
-
-void handleKeyboardEvent(SDL_Event *pointerEvent)
-{
-	// Handle key press
-	if (pointerEvent->key.state == SDL_PRESSED)
+	void handleGameControllerEvent(SDL_Event *pointerEvent)
 	{
-		switch (pointerEvent->key.keysym.scancode)
+		// Analog controls
+		if (pointerEvent->type == SDL_CONTROLLERAXISMOTION)
 		{
-			case SDL_SCANCODE_UP:
-				_handleGoUpKeyPress();
-				break;
-				
-			case SDL_SCANCODE_DOWN:
-				_handleGoDownKeyPress();
-				break;
-				
-			case SDL_SCANCODE_LEFT:
-				_handleGoLeftKeyPress();
-				break;
-				
-			case SDL_SCANCODE_RIGHT:
-				_handleGoRightKeyPress();
-				break;
-				
-			case SDL_SCANCODE_SPACE:
-				_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = 1;
-				break;
-				
-			case SDL_SCANCODE_LCTRL:
-				_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = 1;
-				break;
-				
-			case SDL_SCANCODE_ESCAPE:
-				_isKeyPressed[KEY_ID_PAUSE_GAME] = 1;
-				break;
-				
-			case SDL_SCANCODE_RETURN:
-				_isKeyPressed[KEY_ID_MENU_SELECT] = 1;
-				break;
-				
-			default:
-				break;
-		}
-	}
-	// Handle key release
-	else
-	{
-		switch (pointerEvent->key.keysym.scancode)
-		{
-			case SDL_SCANCODE_UP:
-				_isKeyPressed[KEY_ID_GO_UP] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 0;
-				break;
-				
-			case SDL_SCANCODE_DOWN:
-				_isKeyPressed[KEY_ID_GO_DOWN] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 0;
-				break;
-				
-			case SDL_SCANCODE_LEFT:
-				_isKeyPressed[KEY_ID_GO_LEFT] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 1;
-				break;
-				
-			case SDL_SCANCODE_RIGHT:
-				_isKeyPressed[KEY_ID_GO_RIGHT] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 1;
-				break;
-				
-			case SDL_SCANCODE_SPACE:
-				_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = 0;
-				break;
-				
-			case SDL_SCANCODE_LCTRL:
-				_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = 0;
-				break;
+			// Cache values
+			unsigned char axis = pointerEvent->caxis.axis;
+			short value = pointerEvent->caxis.value;
 			
-			case SDL_SCANCODE_ESCAPE:
-				_isKeyPressed[KEY_ID_PAUSE_GAME] = 0;
-				break;
-				
-			case SDL_SCANCODE_RETURN:
-				_isKeyPressed[KEY_ID_MENU_SELECT] = 0;
-				break;
-				
-			default:
-				break;
-		}
-	}
-}
-
-void handleGameControllerEvent(SDL_Event *pointerEvent)
-{
-	unsigned char axis;
-	short value;
-	
-	// Analog controls
-	if (pointerEvent->type == SDL_CONTROLLERAXISMOTION)
-	{
-		// Cache values
-		axis = pointerEvent->caxis.axis;
-		value = pointerEvent->caxis.value;
-		
-		// Left stick X axis
-		if ((axis == SDL_CONTROLLER_AXIS_LEFTX) || (axis == SDL_CONTROLLER_AXIS_RIGHTX))
-		{
-			if (value <= -CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoLeftKeyPress();
-			else if (value >= CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoRightKeyPress();
-			else
+			// Left stick X axis
+			if ((axis == SDL_CONTROLLER_AXIS_LEFTX) || (axis == SDL_CONTROLLER_AXIS_RIGHTX))
 			{
-				_isKeyPressed[KEY_ID_GO_LEFT] = 0;
-				_isKeyPressed[KEY_ID_GO_RIGHT] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 1;
+				if (value <= -CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoLeftKeyPress();
+				else if (value >= CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoRightKeyPress();
+				else
+				{
+					_isKeyPressed[KEY_ID_GO_LEFT] = false;
+					_isKeyPressed[KEY_ID_GO_RIGHT] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = true;
+				}
+			}
+			
+			// Left stick Y axis
+			if ((axis == SDL_CONTROLLER_AXIS_LEFTY) || (axis == SDL_CONTROLLER_AXIS_RIGHTY))
+			{
+				if (value <= -CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoUpKeyPress();
+				else if (value >= CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoDownKeyPress();
+				else
+				{
+					_isKeyPressed[KEY_ID_GO_UP] = false;
+					_isKeyPressed[KEY_ID_GO_DOWN] = false;
+					_isLastPressedDirectionKeyOnVerticalAxis = false;
+				}
 			}
 		}
-		
-		// Left stick Y axis
-		if ((axis == SDL_CONTROLLER_AXIS_LEFTY) || (axis == SDL_CONTROLLER_AXIS_RIGHTY))
+		// Button press events
+		else if (pointerEvent->type == SDL_CONTROLLERBUTTONDOWN)
 		{
-			if (value <= -CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoUpKeyPress();
-			else if (value >= CONFIGURATION_GAMEPLAY_JOYSTICK_ANALOG_THRESHOLD) _handleGoDownKeyPress();
-			else
+			switch (pointerEvent->cbutton.button)
 			{
-				_isKeyPressed[KEY_ID_GO_UP] = 0;
-				_isKeyPressed[KEY_ID_GO_DOWN] = 0;
-				_isLastPressedDirectionKeyOnVerticalAxis = 0;
+				case SDL_CONTROLLER_BUTTON_A:
+					_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = true;
+					break;
+					
+				case SDL_CONTROLLER_BUTTON_B:
+					_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = true;
+					break;
+					
+				case SDL_CONTROLLER_BUTTON_START:
+					_isKeyPressed[KEY_ID_PAUSE_GAME] = true;
+					break;
+					
+				default:
+					break;
+			}
+		}
+		// Button release events
+		else if (pointerEvent->type == SDL_CONTROLLERBUTTONUP)
+		{
+			switch (pointerEvent->cbutton.button)
+			{
+				case SDL_CONTROLLER_BUTTON_A:
+					_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = false;
+					break;
+					
+				case SDL_CONTROLLER_BUTTON_B:
+					_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = false;
+					break;
+					
+				case SDL_CONTROLLER_BUTTON_START:
+					_isKeyPressed[KEY_ID_PAUSE_GAME] = false;
+					break;
+					
+				default:
+					break;
 			}
 		}
 	}
-	// Button press events
-	else if (pointerEvent->type == SDL_CONTROLLERBUTTONDOWN)
-	{
-		switch (pointerEvent->cbutton.button)
-		{
-			case SDL_CONTROLLER_BUTTON_A:
-				_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = 1;
-				break;
-				
-			case SDL_CONTROLLER_BUTTON_B:
-				_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = 1;
-				break;
-				
-			case SDL_CONTROLLER_BUTTON_START:
-				_isKeyPressed[KEY_ID_PAUSE_GAME] = 1;
-				break;
-				
-			default:
-				break;
-		}
-	}
-	// Button release events
-	else if (pointerEvent->type == SDL_CONTROLLERBUTTONUP)
-	{
-		switch (pointerEvent->cbutton.button)
-		{
-			case SDL_CONTROLLER_BUTTON_A:
-				_isKeyPressed[KEY_ID_PRIMARY_SHOOT] = 0;
-				break;
-				
-			case SDL_CONTROLLER_BUTTON_B:
-				_isKeyPressed[KEY_ID_SECONDARY_SHOOT] = 0;
-				break;
-				
-			case SDL_CONTROLLER_BUTTON_START:
-				_isKeyPressed[KEY_ID_PAUSE_GAME] = 0;
-				break;
-				
-			default:
-				break;
-		}
-	}
-}
 
-int isLastPressedDirectionKeyOnVerticalAxis()
-{
-	return _isLastPressedDirectionKeyOnVerticalAxis;
-}
-
+	bool isLastPressedDirectionKeyOnVerticalAxis()
+	{
+		return _isLastPressedDirectionKeyOnVerticalAxis;
+	}
 }
