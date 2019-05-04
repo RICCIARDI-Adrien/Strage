5f17670ea0f1a8e5ae6acad07e635e621b8d5ee2
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-05-04 21:40:27 +0200

    Refactored 'SavegameManager' namespace.

--- Includes/SavegameManager.hpp
+++ Includes/SavegameManager.hpp
@@ -7,53 +7,45 @@
 
 namespace SavegameManager
 {
+	/** All saved items. Don't forget to update _items array default values when adding new members. */
+	typedef enum
+	{
+		SAVEGAME_ITEM_ID_STARTING_LEVEL, //!< The level to load.
+		SAVEGAME_ITEM_ID_PLAYER_MAXIMUM_LIFE_POINTS, //!< The maximum life points amount the player managed to have by taking golden medipacks.
+		SAVEGAME_ITEM_ID_PLAYER_AMMUNITION, //!< How many ammunition the player has.
+		SAVEGAME_ITEM_IDS_COUNT
+	} SavegameItemId;
 
-//-------------------------------------------------------------------------------------------------
-// Public types
-//-------------------------------------------------------------------------------------------------
-/** All saved items. Don't forget to update _items array default values when adding new members. */
-typedef enum
-{
-	SAVEGAME_ITEM_ID_STARTING_LEVEL, //!< The level to load.
-	SAVEGAME_ITEM_ID_PLAYER_MAXIMUM_LIFE_POINTS, //!< The maximum life points amount the player managed to have by taking golden medipacks.
-	SAVEGAME_ITEM_ID_PLAYER_AMMUNITION, //!< How many ammunition the player has.
-	SAVEGAME_ITEM_IDS_COUNT
-} SavegameItemId;
+	/** Load the savegame file content.
+	* @return -1 if the savegame file is missing or the corrupted,
+	* @return 0 if the savegame was successfully loaded.
+	*/
+	int loadSavegame();
 
-//-------------------------------------------------------------------------------------------------
-// Public functions
-//-------------------------------------------------------------------------------------------------
-/** Load the savegame file content.
- * @return -1 if the savegame file is missing or the corrupted,
- * @return 0 if the savegame was successfully loaded.
- */
-int loadSavegame(void);
+	/** Overwrite the current savegame (if any) or create a new savegame file filled with the current savegame values.
+	* @note The function displays error messages if something goes wrong.
+	*/
+	void storeSavegame();
 
-/** Overwrite the current savegame (if any) or create a new savegame file filled with the current savegame values.
- * @note The function displays error messages if something goes wrong.
- */
-void storeSavegame(void);
+	/** Get an item value.
+	* @param itemId The item identifier.
+	* @return The corresponding item value.
+	* @note The function crashes the game if a bad item ID is provided.
+	*/
+	int getSavegameItem(SavegameItemId itemId);
 
-/** Get an item value.
- * @param itemId The item identifier.
- * @return The corresponding item value.
- * @note The function crashes the game if a bad item ID is provided.
- */
-int getSavegameItem(SavegameItemId itemId);
+	/** Set an item value.
+	* @param itemId The item identifier.
+	* @param itemValue The item value.
+	* @note The function crashes the game if a bad item ID is provided.
+	*/
+	void setSavegameItem(SavegameItemId itemId, int itemValue);
 
-/** Set an item value.
- * @param itemId The item identifier.
- * @param itemValue The item value.
- * @note The function crashes the game if a bad item ID is provided.
- */
-void setSavegameItem(SavegameItemId itemId, int itemValue);
-
-/** Tell whether a savegame file is present.
- * @return false if no savegame file is available,
- * @return true if a savegame file has been found.
- */
-bool isSavegamePresent();
-
+	/** Tell whether a savegame file is present.
+	* @return false if no savegame file is available,
+	* @return true if a savegame file has been found.
+	*/
+	bool isSavegamePresent();
 }
 
 #endif
--- Sources/SavegameManager.cpp
+++ Sources/SavegameManager.cpp
@@ -1,5 +1,5 @@
 /** @file SavegameManager.cpp
- * @see SavegameManager.hpp for description.
+ * See SavegameManager.hpp for description.
  * @author Adrien RICCIARDI
  */
 #include <cassert>
@@ -11,100 +11,86 @@
 
 namespace SavegameManager
 {
-
-//-------------------------------------------------------------------------------------------------
-// Private variables
-//-------------------------------------------------------------------------------------------------
-/** All savegame items. */
-static int _items[SAVEGAME_ITEM_IDS_COUNT] = // Set default values to use in case the savegame is not found or when a new game is started
-{
-	0,
-	CONFIGURATION_GAMEPLAY_PLAYER_DEFAULT_LIFE_POINTS_AMOUNT,
-	CONFIGURATION_GAMEPLAY_PLAYER_DEFAULT_AMMUNITION_AMOUNT
-};
-
-//-------------------------------------------------------------------------------------------------
-// Public functions
-//-------------------------------------------------------------------------------------------------
-int loadSavegame(void)
-{
-	FILE *pointerFile;
-	int i;
-	
-	// Try to open the file
-	pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "r");
-	if (pointerFile == NULL)
+	/** All savegame items. */
+	static int _items[SAVEGAME_ITEM_IDS_COUNT] = // Set default values to use in case the savegame is not found or when a new game is started
 	{
-		LOG_INFORMATION("No savegame file found.");
-		return -1;
-	}
-	
-	// Try to load all items
-	for (i = 0; i < SAVEGAME_ITEM_IDS_COUNT; i++)
+		0,
+		CONFIGURATION_GAMEPLAY_PLAYER_DEFAULT_LIFE_POINTS_AMOUNT,
+		CONFIGURATION_GAMEPLAY_PLAYER_DEFAULT_AMMUNITION_AMOUNT
+	};
+
+	int loadSavegame()
 	{
-		// Load the next item
-		if (fscanf(pointerFile, "%d", &_items[i]) != 1)
+		// Try to open the file
+		FILE *pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "r");
+		if (pointerFile == NULL)
 		{
-			LOG_INFORMATION("Failed to load item %u. Make sure " CONFIGURATION_SAVEGAME_FILE_NAME " is correct or delete this file to allow the game to run.", i);
-			fclose(pointerFile);
+			LOG_INFORMATION("No savegame file found.");
 			return -1;
 		}
-		else LOG_DEBUG("Loaded item %u : %d", i, _items[i]);
+		
+		// Try to load all items
+		for (int i = 0; i < SAVEGAME_ITEM_IDS_COUNT; i++)
+		{
+			// Load the next item
+			if (fscanf(pointerFile, "%d", &_items[i]) != 1)
+			{
+				LOG_INFORMATION("Failed to load item %u. Make sure " CONFIGURATION_SAVEGAME_FILE_NAME " is correct or delete this file to allow the game to run.", i);
+				fclose(pointerFile);
+				return -1;
+			}
+			else LOG_DEBUG("Loaded item %u : %d", i, _items[i]);
+		}
+		
+		fclose(pointerFile);
+		return 0;
 	}
-	
-	fclose(pointerFile);
-	return 0;
-}
 
-void storeSavegame(void)
-{
-	FILE *pointerFile;
-	int i;
-	
-	// Try to open the file
-	pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "w");
-	if (pointerFile == NULL)
+	void storeSavegame()
 	{
-		LOG_INFORMATION("Could not open savegame file in write mode.");
-		return;
+		// Try to open the file
+		FILE *pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "w");
+		if (pointerFile == NULL)
+		{
+			LOG_INFORMATION("Could not open savegame file in write mode.");
+			return;
+		}
+		
+		// Write all items in order
+		for (int i = 0; i < SAVEGAME_ITEM_IDS_COUNT; i++)
+		{
+			if (fprintf(pointerFile, "%d\n", _items[i]) < 0) LOG_ERROR("Failed to write savegame item %d.", i);
+		}
+		
+		fclose(pointerFile);
 	}
-	
-	// Write all items in order
-	for (i = 0; i < SAVEGAME_ITEM_IDS_COUNT; i++)
+
+	int getSavegameItem(SavegameItemId itemId)
 	{
-		if (fprintf(pointerFile, "%d\n", _items[i]) < 0) LOG_ERROR("Failed to write savegame item %d.", i);
+		// Make sure the ID is correct
+		assert(itemId < SAVEGAME_ITEM_IDS_COUNT);
+		
+		return _items[itemId];
 	}
-	
-	fclose(pointerFile);
-}
 
-int getSavegameItem(SavegameItemId itemId)
-{
-	// Make sure the ID is correct
-	assert(itemId < SAVEGAME_ITEM_IDS_COUNT);
-	
-	return _items[itemId];
-}
+	void setSavegameItem(SavegameItemId itemId, int itemValue)
+	{
+		// Make sure the ID is correct
+		assert(itemId < SAVEGAME_ITEM_IDS_COUNT);
+		
+		_items[itemId] = itemValue;
+	}
 
-void setSavegameItem(SavegameItemId itemId, int itemValue)
-{
-	// Make sure the ID is correct
-	assert(itemId < SAVEGAME_ITEM_IDS_COUNT);
-	
-	_items[itemId] = itemValue;
-}
-
-bool isSavegamePresent()
-{
-	// Try to open the file
-	FILE *pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "r");
-	
-	// No file was found
-	if (pointerFile == NULL) return false;
-	
-	// The file is present
-	fclose(pointerFile);
-	return true;
-}
-
+	bool isSavegamePresent()
+	{
+		// Try to open the file
+		FILE *pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_SAVEGAME_FILE_NAME), "r");
+		
+		// No file was found
+		if (pointerFile == NULL) return false;
+		
+		// The file is present
+		fclose(pointerFile);
+		return true;
+	}
 }
