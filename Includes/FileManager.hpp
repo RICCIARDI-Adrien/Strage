1e4c60a66ef44b387914d88a02598be5d7f9b0a1
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-05-04 21:08:15 +0200

    Refactored 'FileManager' namespace.

--- Includes/FileManager.hpp
+++ Includes/FileManager.hpp
@@ -7,22 +7,17 @@
 
 namespace FileManager
 {
+	/** Retrieve the game base directory.
+	* @return 0 on success,
+	* @return -1 if an error occurred.
+	*/
+	int initialize();
 
-//-------------------------------------------------------------------------------------------------
-// Functions
-//-------------------------------------------------------------------------------------------------
-/** Retrieve the game base directory.
- * @return 0 on success,
- * @return -1 if an error occurred.
- */
-int initialize();
-
-/** Build an absolute path leading to the provided file (only on platform that use dynamic paths, on other platforms the provided file name is returned as-is because relative paths work).
- * @param pointerStringFileName The file to get absolute path. The file can be preceded by directories.
- * @return A static string containing the file absolute path.
- */
-const char *getFilePath(const char *pointerStringFileName);
-
+	/** Build an absolute path leading to the provided file (only on platform that use dynamic paths, on other platforms the provided file name is returned as-is because relative paths work).
+	* @param pointerStringFileName The file to get absolute path. The file can be preceded by directories.
+	* @return A static string containing the file absolute path.
+	*/
+	const char *getFilePath(const char *pointerStringFileName);
 }
 
 #endif
--- Sources/FileManager.cpp
+++ Sources/FileManager.cpp
@@ -1,5 +1,5 @@
 /** @file FileManager.cpp
- * @see FileManager.hpp for description.
+ * See FileManager.hpp for description.
  * @author Adrien RICCIARDI
  */
 #include <cstring>
@@ -9,63 +9,55 @@
 
 namespace FileManager
 {
-
-//-------------------------------------------------------------------------------------------------
-// Private variables
-//-------------------------------------------------------------------------------------------------
-#ifdef CONFIGURATION_BUILD_FOR_MACOS
-	/** Game base path (it leads to bundle/Content/Resources on macOS bundle). */
-	static char stringBasePath[2048];
-#endif
-
-//-------------------------------------------------------------------------------------------------
-// Public functions
-//-------------------------------------------------------------------------------------------------
-int initialize()
-{
-	// Only macOS needs that, other platforms use a relative path
 	#ifdef CONFIGURATION_BUILD_FOR_MACOS
-		char *pointerStringPath;
-		unsigned int pathLength;
-		
-		// Retrieve application path
-		pointerStringPath = SDL_GetBasePath();
-		if (pointerStringPath == NULL)
-		{
-			LOG_ERROR("Failed to obtain the application base path (%s).", SDL_GetError());
-			return -1;
-		}
-		
-		// Make sure it is not too long
-		pathLength = strlen(pointerStringPath);
-		if (pathLength >= sizeof(stringBasePath))
-		{
-			LOG_ERROR("Base path is too long (%u characters).", pathLength);
+		/** Game base path (it leads to bundle/Content/Resources on macOS bundle). */
+		static char stringBasePath[2048];
+	#endif
+
+	int initialize()
+	{
+		// Only macOS needs that, other platforms use a relative path
+		#ifdef CONFIGURATION_BUILD_FOR_MACOS
+			char *pointerStringPath;
+			unsigned int pathLength;
+			
+			// Retrieve application path
+			pointerStringPath = SDL_GetBasePath();
+			if (pointerStringPath == NULL)
+			{
+				LOG_ERROR("Failed to obtain the application base path (%s).", SDL_GetError());
+				return -1;
+			}
+			
+			// Make sure it is not too long
+			pathLength = strlen(pointerStringPath);
+			if (pathLength >= sizeof(stringBasePath))
+			{
+				LOG_ERROR("Base path is too long (%u characters).", pathLength);
+				SDL_free(pointerStringPath);
+				return -1;
+			}
+			
+			// Internally store base path
+			strcpy(stringBasePath, pointerStringPath);
 			SDL_free(pointerStringPath);
-			return -1;
-		}
-		
-		// Internally store base path
-		strcpy(stringBasePath, pointerStringPath);
-		SDL_free(pointerStringPath);
-		LOG_DEBUG("Base path : %s", stringBasePath);
-	#endif
+			LOG_DEBUG("Base path : %s", stringBasePath);
+		#endif
 
-	return 0;
-}
+		return 0;
+	}
 
-const char *getFilePath(const char *pointerStringFileName)
-{
-	#ifdef CONFIGURATION_BUILD_FOR_MACOS
-		static char stringPath[16384];
-		
-		// Build the path
-		strcpy(stringPath, stringBasePath); // Base path has always a terminating separator (this is specified by SDL documentation)
-		strcat(stringPath, pointerStringFileName);
-		return stringPath;
-	#else
-		return pointerStringFileName;
-	#endif
-}
-
+	const char *getFilePath(const char *pointerStringFileName)
+	{
+		#ifdef CONFIGURATION_BUILD_FOR_MACOS
+			static char stringPath[16384];
+			
+			// Build the path
+			strcpy(stringPath, stringBasePath); // Base path has always a terminating separator (this is specified by SDL documentation)
+			strcat(stringPath, pointerStringFileName);
+			return stringPath;
+		#else
+			return pointerStringFileName;
+		#endif
+	}
 }
