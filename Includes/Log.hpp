08d0a72bb9118abade4b79e0027195cc1295987e
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-11-10 13:20:18 +0100

    Finished new logging system implementation. All log messages are now redirected to a file.

--- Includes/Log.hpp
+++ Includes/Log.hpp
@@ -15,14 +15,14 @@
  * @param stringMessage The message to display.
  * @note Error logs can't be disabled.
  */
-#define LOG_ERROR(stringMessage, ...) printf("\033[31m[%s:%s():%d ERROR] " stringMessage "\033[0m\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(stringMessage, ...) Log::appendMessage("ERROR [%s:%s():%d] " stringMessage "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 /** @def LOG_INFORMATION() Display a yellow information message on the console.
  * @param stringMessage The message to display.
  * @note Information log are enabled with a log level of 1 or more.
  */
 #if CONFIGURATION_LOG_LEVEL >= 1
-	#define LOG_INFORMATION(stringMessage, ...) printf("\033[33m[%s:%s():%d INFORMATION] " stringMessage "\033[0m\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+	#define LOG_INFORMATION(stringMessage, ...) Log::appendMessage("INFORMATION [%s:%s():%d] " stringMessage "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 	#define LOG_INFORMATION(stringMessage, ...) {} // Replace by an empty scope instead of nothing in case the log is the only instruction of an if, which would have an empty body if logs are disabled
 #endif
@@ -32,7 +32,7 @@
  * @note Debug log are enabled with a log level of 2.
  */
 #if CONFIGURATION_LOG_LEVEL >= 2
-	#define LOG_DEBUG(stringMessage, ...) printf("\033[32m[%s:%s():%d DEBUG] " stringMessage "\033[0m\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+	#define LOG_DEBUG(stringMessage, ...) Log::appendMessage("DEBUG [%s:%s():%d] " stringMessage "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 	#define LOG_DEBUG(stringMessage, ...) {} // Replace by an empty scope instead of nothing in case the log is the only instruction of an if, which would have an empty body if logs are disabled
 #endif
--- Sources/Log.cpp
+++ Sources/Log.cpp
@@ -3,9 +3,11 @@
  * @author Adrien RICCIARDI
  */
 #include <cerrno>
+#include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <FileManager.hpp>
 #include <Log.hpp>
 
 namespace Log
@@ -23,7 +25,7 @@ namespace Log
 	int initialize()
 	{
 		// Try to open the file
-		_pointerFile = fopen(CONFIGURATION_LOG_FILE_NAME, "w");
+		_pointerFile = fopen(FileManager::getFilePath(CONFIGURATION_LOG_FILE_NAME), "w");
 		if (_pointerFile == NULL)
 		{
 			printf("Error : failed to open log file '%s' in write-only mode (%s).\n", CONFIGURATION_LOG_FILE_NAME, strerror(errno));
@@ -35,5 +37,17 @@ namespace Log
 		
 		fprintf(_pointerFile, "Logging system initialized.\n");
 		return 0;
+	}
+	
+	void appendMessage(const char *pointerMessageFormat, ...)
+	{
+		// Prepare variable arguments list
+		va_list argumentsList;
+		va_start(argumentsList, pointerMessageFormat);
+		
+		// Append the message
+		vfprintf(_pointerFile, pointerMessageFormat, argumentsList);
+		
+		va_end(argumentsList);
 	}
 }
